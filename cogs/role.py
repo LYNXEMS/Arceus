ef32315fed9d0211b05bd00cda5dd8e091ede317
redpfire <redpixafire@gmail.com>
2018-01-22 00:42:44 +0100

    optymalizacja funkcji kolorów

--- cogs/role.py
+++ cogs/role.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from sys import argv
+import sys
 
 class Nsfw:
     """
@@ -8,9 +8,13 @@ class Nsfw:
     """
     def __init__(self, bot):
         self.bot = bot
+
+        self.get_colors = True
+        self.colors = [["❤Z","zielony"],["❤N","niebieski"],["❤Ż","żółty"],["❤R","różowy"],["❤C","czerwony"]]
+
         print('Addon "{}" loaded'.format(self.__class__.__name__))
 
-    
+
     @commands.has_permissions(change_nickname=True)
     @commands.command(hidden=True, pass_context=True)
     async def nsfw(self, ctx):
@@ -38,7 +42,7 @@ class Nsfw:
             except:
                 await self.bot.say("Brak odpowiedzi lub niepoprawna odpowiedź, jeśli chcesz ponownie spróbować uzyskać dostęp, ponownie wpiszk komendę.")
                 return
-				
+
 
     @commands.command(pass_context=True)
     async def kod(self, ctx, kod):
@@ -70,47 +74,40 @@ class Nsfw:
         else:
             await self.bot.send_message(author, "{} kod nie jest wlasciwy.".format(kod))
 
+    async def clear_colors(self, usr):
+        r = [item[0] for item in self.colors]
+        await self.bot.remove_roles(usr, *r)
+
+    async def update_color(self, usr, color):
+        await self.clear_colors(usr)
+        await self.bot.add_roles(usr, self.get_color(color))
+
+    def get_color(self, color):
+        a = [item[0] for item in self.colors if item[1] == color]
+        return a[0]
+
     @commands.command(pass_context=True)
     async def kolor(self, ctx, kolor):
         """Zmienia kolor twojego nicku. Tylko znani. Dostępne kolory to czerwony, zielony, żółty, różowy i niebieski."""
         server = ctx.message.server
         author = ctx.message.author
+        debug  = False
+        if self.get_colors:
+            for i, color in enumerate(self.colors):
+                self.colors[i][0] = discord.utils.get(server.roles, name=color[0])
+            self.get_colors = False
+
         if discord.utils.get(server.roles, name="Znany") in author.roles:
-            if kolor == "czerwony":
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Z"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤N"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Ż"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤R"))
-                await self.bot.add_roles(author, discord.utils.get(server.roles, name="❤C"))
-                await self.bot.say("Kolor Zmieniony")
-            if kolor == "zielony":
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤C"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤N"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Ż"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤R"))
-                await self.bot.add_roles(author, discord.utils.get(server.roles, name="❤Z"))
-                await self.bot.say("Kolor Zmieniony")
-            if kolor == "żółty":
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤C"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤N"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Z"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤R"))
-                await self.bot.add_roles(author, discord.utils.get(server.roles, name="❤Ż"))
-                await self.bot.say("Kolor Zmieniony")
-            if kolor == "różowy":
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤C"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤N"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Z"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Ż"))
-                await self.bot.add_roles(author, discord.utils.get(server.roles, name="❤R"))
-                await self.bot.say("Kolor Zmieniony")
-            if kolor == "niebieski":
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤C"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤R"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Z"))
-                await self.bot.remove_roles(author, discord.utils.get(server.roles, name="❤Ż"))
-                await self.bot.add_roles(author, discord.utils.get(server.roles, name="❤N"))
-                await self.bot.say("Kolor Zmieniony")
+            try:
+                await self.update_color(author, kolor)
+                await self.bot.say("Kolor zmieniony.")
+            except BaseException as e:
+                await self.bot.say("Podany kolor jest nieprawidłowy.")
+                if debug:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    await self.bot.say("EXCEPTION: {} on line {}".format(str(e), exc_tb.tb_lineno))
+                pass
+
             else:
                 return
         else:
